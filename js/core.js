679201bc4edda7bc57f9c93632a20c9c69828139
aldjanaliev <aldjanaliev@gmail.com>
2021-12-15 01:36:59 +0300

    js file

--- js/core.js
+++ js/core.js
@@ -28,8 +28,41 @@ $(document).ready(function(){
             
       });
 
-			$('.burger-btn').on('click', function(event){
-				$(this).toggleClass('burger-active');
-				$('.menu-burger').slideToggle(300);
+      $('.burger-btn').on('click', function(event){
+        $(this).toggleClass('burger-active');
+        $('.menu-burger').slideToggle(300);
+      })
+			$('.catalog_burger').on('click', function(event){
+        $(this).toggleClass('catalog_burger-active');
+        $('.catalog_in-nav-wrap').slideToggle(300);
+      })
+
+			$('.catalog_in-link').on('click', function(event){
+        if(!$(this).hasClass('catalog_in-link__active')){
+					$('.catalog_in-link').removeClass('catalog_in-link__active');
+					$(this).addClass('catalog_in-link__active');
+				}
+      })
+
+			document.querySelector('.order_counter-value').addEventListener('input', 
+				function(e){
+					this.value = this.value.replace(/[^\d.]/g, '');
+				}
+			)
+			const btns = document.querySelectorAll('.order_couner');
+			btns.forEach(btn => {
+				btn.addEventListener('click', function(){
+					const direction = this.dataset.direction;
+					const inp = this.parentElement.querySelector('.order_counter-value');
+					const currentValue = +inp.value;
+					let newValue;
+					if(direction === 'plus'){
+						newValue = currentValue + 1
+					} else{
+						newValue = currentValue - 1 > 0 ? currentValue - 1 : 0;
+					}
+					inp.value = newValue
+				})
 			})
+
 });
